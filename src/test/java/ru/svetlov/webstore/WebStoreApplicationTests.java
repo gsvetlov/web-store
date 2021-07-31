a28c60776d7edf3e53913bce54fb3bcc27c243f6
Georg Svetlov <gsvetlov@gmail.com>
2021-07-31 15:08:56 +0300

    initial commit

---
+++ src/test/java/ru/svetlov/webstore/WebStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.svetlov.webstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
