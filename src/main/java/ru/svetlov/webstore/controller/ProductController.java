cc8b61bd03ec4824df3234a445af30cab050426b
Georg Svetlov <gsvetlov@gmail.com>
2021-07-31 23:24:51 +0300

    форма создания продукта и привязки в контроллере

--- src/main/java/ru/svetlov/webstore/controller/ProductController.java
+++ src/main/java/ru/svetlov/webstore/controller/ProductController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import ru.svetlov.webstore.domain.Product;
 import ru.svetlov.webstore.service.ProductService;
 
 import java.util.List;
@@ -20,20 +19,25 @@ public class ProductController {
 
     @GetMapping("/product/{id}")
     public String getProductById(@PathVariable Long id, Model model){
-        model.addAttribute(productService.getById(id));
+        model.addAttribute("product", productService.getById(id));
         return "product_info";
     }
 
-    @GetMapping("/product")
+    @GetMapping("/products")
     public String getAllProducts(Model model){
         model.addAttribute("products", productService.getAll());
         return "products";
     }
 
     @GetMapping("/product/create")
+    public String showCreateProductForm(){
+        return "create_product";
+    }
+
+    @PostMapping("/product/create")
     public String createProduct(@RequestParam("title") String title, @RequestParam("cost") double cost){
         boolean result = productService.create(title, cost);
-        if (result) return "redirect:/product";
+        if (result) return "redirect:/products";
         return "create_fail";
     }
 }
