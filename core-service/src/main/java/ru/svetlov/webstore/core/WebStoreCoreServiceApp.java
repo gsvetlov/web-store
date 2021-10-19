4c3980bb5e509e70279ca8fb88e7b4f9b7d41b2f
Georg Svetlov <gsvetlov@gmail.com>
2021-10-19 22:15:06 +0300

    GatewayServiceApp

--- core-service/src/main/java/ru/svetlov/webstore/core/WebStoreCoreServiceApp.java
+++ core-service/src/main/java/ru/svetlov/webstore/core/WebStoreCoreServiceApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebStoreApplication {
+public class WebStoreCoreServiceApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebStoreApplication.class, args);
+        SpringApplication.run(WebStoreCoreServiceApp.class, args);
     }
 
 }
---
+++ gateway-service/src/main/java/ru/svetlov/webstore/gateway/GatewayServiceApp.java
@@ -0,0 +1,11 @@
+package ru.svetlov.webstore.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApp.class, args);
+    }
+}
