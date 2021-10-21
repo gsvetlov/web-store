d19ce804d2d09e08482e87d3b8fce6647bf53164
SvGA <gsvetlov@gmail.com>
2021-10-22 00:20:23 +0300

    транзакция createOrder отделена от вызовов сервисов, исправлен маппинг CartItemsToOrderItems, переработан getOrderById

--- order-service/src/main/java/ru/svetlov/webapp/order/service/impl/OrderServiceImpl.java
+++ order-service/src/main/java/ru/svetlov/webapp/order/service/impl/OrderServiceImpl.java
@@ -10,12 +10,9 @@ import ru.svetlov.webapp.order.integration.CartServiceClient;
 import ru.svetlov.webapp.order.repository.OrderRepository;
 import ru.svetlov.webapp.order.service.OrderService;
 import ru.svetlov.webstore.api.dtos.CartDto;
-import ru.svetlov.webstore.api.dtos.CartItemDto;
 import ru.svetlov.webstore.api.dtos.OrderDetailsDto;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -24,41 +21,39 @@ import java.util.stream.Collectors;
 public class OrderServiceImpl implements OrderService {
     private final OrderRepository orderRepository;
     private final CartServiceClient cartServiceClient;
-//    private final ProductService productService;
 
     @Override
     public Optional<Order> getOrderById(Long orderId, Long userId) {
         Optional<Order> order = orderRepository.findOrderById(orderId);
-        //TODO: проверка, что заказ принадлежит пользователю, иначе Optional.empty()
+        return order.isPresent() && order.get().getUserId().equals(userId)
+                ? order : Optional.empty();
+    }
+
+    @Override
+    public Order createOrder(Long userId, OrderDetailsDto details) {
+        CartDto cart = cartServiceClient.getCart(userId);
+        List<OrderItem> orderItems = mapCartToOrderItems(cart);
+        Order order = create(userId, details, orderItems);
+        cartServiceClient.clear(cart.getCartId());
         return order;
     }
 
     @Transactional
-    @Override
-    public Order createOrder(Long userId, OrderDetailsDto details) {
-        CartDto cart = cartServiceClient.getCart(userId);
+    private Order create(Long userId, OrderDetailsDto details, List<OrderItem> orderItems) {
         Order order = new Order(userId);
-        order.setOrderItems(mapCartToOrderItems(cart));
+        order.setOrderItems(orderItems);
         order.setDetails(OrderDetails.of(details.getAddress(), details.getPhone()));
         orderRepository.save(order);
-        cartServiceClient.clear(cart.getCartId()); // TODO: fix cart clear
         return order;
     }
 
     private List<OrderItem> mapCartToOrderItems(CartDto cart) {
-        Map<Long, Long> products = mapCartIdsToProducts(cart);
         return cart.getContents().stream()
                 .map(ci -> OrderItem.of(
-                        products.get(ci.getProductId()),
+                        ci.getProductId(),
                         ci.getQuantity(),
                         ci.getProductPrice(),
                         ci.getItemSum()))
                 .collect(Collectors.toList());
-    }
-
-    private Map<Long, Long> mapCartIdsToProducts(CartDto cart) {
-        List<Long> ids = cart.getContents().stream().map(CartItemDto::getProductId).collect(Collectors.toList());
-        return Collections.EMPTY_MAP; // TODO: fix product-service integration
-        // productService.getAllByIdIn(ids).stream().collect(Collectors.toMap(Long::getLong, Function.identity()));
     }
 }
