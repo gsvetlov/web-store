ed216d3a8a3f5dcfd856bdc9c1aa12abc290145b
Georg Svetlov <gsvetlov@gmail.com>
2021-10-23 13:05:03 +0300

    front-ui

---
+++ front-ui/src/main/java/ru/svetlov/webstore/front/WebStoreUiApplication.java
@@ -0,0 +1,11 @@
+package ru.svetlov.webstore.front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStoreUiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebStoreUiApplication.class, args);
+    }
+}
