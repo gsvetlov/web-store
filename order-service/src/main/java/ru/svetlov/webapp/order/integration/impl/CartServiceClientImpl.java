aa7560a9914a86a5a42dab5fd79bd8a1e28263c0
SvGA <gsvetlov@gmail.com>
2021-10-28 23:31:40 +0300

    hw11 замена RestTemplate на WebClient

--- order-service/src/main/java/ru/svetlov/webapp/order/integration/impl/CartServiceClientImpl.java
+++ order-service/src/main/java/ru/svetlov/webapp/order/integration/impl/CartServiceClientImpl.java
@@ -3,20 +3,20 @@ package ru.svetlov.webapp.order.integration.impl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import ru.svetlov.webapp.order.integration.CartServiceClient;
 import ru.svetlov.webstore.api.dtos.CartDto;
 
 @Service
 @RequiredArgsConstructor
 public class CartServiceClientImpl implements CartServiceClient {
-    private final RestTemplate restTemplate;
+    private final WebClient webClient;
     @Value("${integration.service.cart.uri}")
     private String cartServiceUri;
 
     @Override
     public CartDto getCart(Long userId) {
-        return restTemplate.getForEntity(cartServiceUri, CartDto.class, userId).getBody();
+        return webClient.getForEntity(cartServiceUri, CartDto.class, userId).getBody();
     }
 
     @Override
